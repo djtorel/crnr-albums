e2f45b57794011740f1387da76044c3e3afe0d9f
Dominic Torruellas <dtorruellas@gmail.com>
2017-07-08 21:22:51 -0700

    Add album list component

--- App.js
+++ App.js
@@ -1,15 +1,19 @@
 // Import a library to help create a component
 import React from 'react';
-import { AppRegistry } from 'react-native';
+import { AppRegistry, View } from 'react-native';
 
-import Header from './src/components/header';
+import Header from './src/components/Header';
+import AlbumList from './src/components/AlbumList';
 
 
 // Create a component
 export default class App extends React.Component {
   render() {
     return (
-      <Header headerText={'Albums'} />
+      <View>
+        <Header headerText={'Albums'} />
+        <AlbumList />
+      </View>
     );
   }
 }
