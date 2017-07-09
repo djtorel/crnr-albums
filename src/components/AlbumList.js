10a6c03eeee222f1975069b799bb9b5b97fb87bf
Dominic Torruellas <dtorruellas@gmail.com>
2017-07-08 22:35:08 -0700

    Add AlbumDetail, Card and CardItem for styling, ScrollView to root element

--- src/components/AlbumList.js
+++ src/components/AlbumList.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { ScrollView } from 'react-native';
 import axios from 'axios';
+
+import AlbumDetail from './AlbumDetail';
 
 class AlbumList extends Component {
   state = {
@@ -14,15 +16,15 @@ class AlbumList extends Component {
 
   renderAlbums() {
     return this.state.albums.map(album =>
-      <Text key={album.title}>{album.title}</Text>
+      <AlbumDetail key={album.title} album={album} />
     );
   }
 
   render() {
     return (
-      <View>
+      <ScrollView>
         {this.renderAlbums()}
-      </View>
+      </ScrollView>
     );
   }
 }
